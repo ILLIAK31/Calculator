f780bf818584eb168cd1b81039a720d7c8a04609
Illia <karma@MYPC>
2023-11-11 18:38:40 +0100

    Add project files.

---
+++ App4b(Illia Karmazin)/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace App4b_Illia_Karmazin_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            button10 = new System.Windows.Forms.Button();
+            button11 = new System.Windows.Forms.Button();
+            button12 = new System.Windows.Forms.Button();
+            button13 = new System.Windows.Forms.Button();
+            button14 = new System.Windows.Forms.Button();
+            button15 = new System.Windows.Forms.Button();
+            button16 = new System.Windows.Forms.Button();
+            button17 = new System.Windows.Forms.Button();
+            button18 = new System.Windows.Forms.Button();
+            button19 = new System.Windows.Forms.Button();
+            button20 = new System.Windows.Forms.Button();
+            button21 = new System.Windows.Forms.Button();
+            button22 = new System.Windows.Forms.Button();
+            button23 = new System.Windows.Forms.Button();
+            button24 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(14, 244);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(89, 68);
+            button1.TabIndex = 0;
+            button1.Text = "%";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(109, 244);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(89, 68);
+            button2.TabIndex = 1;
+            button2.Text = "CE";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button3.Location = new System.Drawing.Point(203, 244);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(92, 68);
+            button3.TabIndex = 2;
+            button3.Text = "C";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button4.Location = new System.Drawing.Point(301, 244);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(90, 68);
+            button4.TabIndex = 3;
+            button4.Text = "x";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button5.Location = new System.Drawing.Point(14, 324);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(89, 68);
+            button5.TabIndex = 4;
+            button5.Text = "1/x";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button6.Location = new System.Drawing.Point(109, 324);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(88, 68);
+            button6.TabIndex = 5;
+            button6.Text = "x²";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button7.Location = new System.Drawing.Point(204, 324);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(92, 68);
+            button7.TabIndex = 6;
+            button7.Text = "√x";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button8.Location = new System.Drawing.Point(301, 324);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(90, 68);
+            button8.TabIndex = 7;
+            button8.Text = "÷";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button9.Location = new System.Drawing.Point(14, 404);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(89, 68);
+            button9.TabIndex = 8;
+            button9.Text = "7";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button10.Location = new System.Drawing.Point(109, 404);
+            button10.Name = "button10";
+            button10.Size = new System.Drawing.Size(88, 68);
+            button10.TabIndex = 9;
+            button10.Text = "8";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button11.Location = new System.Drawing.Point(203, 404);
+            button11.Name = "button11";
+            button11.Size = new System.Drawing.Size(92, 68);
+            button11.TabIndex = 10;
+            button11.Text = "9";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button12.Location = new System.Drawing.Point(301, 404);
+            button12.Name = "button12";
+            button12.Size = new System.Drawing.Size(91, 68);
+            button12.TabIndex = 11;
+            button12.Text = "x";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button13.Location = new System.Drawing.Point(15, 484);
+            button13.Name = "button13";
+            button13.Size = new System.Drawing.Size(89, 68);
+            button13.TabIndex = 12;
+            button13.Text = "4";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button14.Location = new System.Drawing.Point(108, 484);
+            button14.Name = "button14";
+            button14.Size = new System.Drawing.Size(89, 68);
+            button14.TabIndex = 13;
+            button14.Text = "5";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button15.Location = new System.Drawing.Point(203, 484);
+            button15.Name = "button15";
+            button15.Size = new System.Drawing.Size(92, 68);
+            button15.TabIndex = 14;
+            button15.Text = "6";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button16.Location = new System.Drawing.Point(301, 484);
+            button16.Name = "button16";
+            button16.Size = new System.Drawing.Size(92, 68);
+            button16.TabIndex = 15;
+            button16.Text = "-";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button17.Location = new System.Drawing.Point(15, 564);
+            button17.Name = "button17";
+            button17.Size = new System.Drawing.Size(89, 68);
+            button17.TabIndex = 16;
+            button17.Text = "1";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button18.Location = new System.Drawing.Point(108, 564);
+            button18.Name = "button18";
+            button18.Size = new System.Drawing.Size(89, 68);
+            button18.TabIndex = 17;
+            button18.Text = "2";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button19.Location = new System.Drawing.Point(203, 564);
+            button19.Name = "button19";
+            button19.Size = new System.Drawing.Size(92, 68);
+            button19.TabIndex = 18;
+            button19.Text = "3";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button20.Location = new System.Drawing.Point(301, 564);
+            button20.Name = "button20";
+            button20.Size = new System.Drawing.Size(92, 68);
+            button20.TabIndex = 19;
+            button20.Text = "+";
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            button21.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button21.Location = new System.Drawing.Point(15, 644);
+            button21.Name = "button21";
+            button21.Size = new System.Drawing.Size(89, 68);
+            button21.TabIndex = 20;
+            button21.Text = "+/-";
+            button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            button22.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button22.Location = new System.Drawing.Point(109, 644);
+            button22.Name = "button22";
+            button22.Size = new System.Drawing.Size(89, 68);
+            button22.TabIndex = 21;
+            button22.Text = "0";
+            button22.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            button23.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button23.Location = new System.Drawing.Point(204, 644);
+            button23.Name = "button23";
+            button23.Size = new System.Drawing.Size(92, 68);
+            button23.TabIndex = 22;
+            button23.Text = ",";
+            button23.UseVisualStyleBackColor = true;
+            // 
+            // button24
+            // 
+            button24.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button24.Location = new System.Drawing.Point(301, 644);
+            button24.Name = "button24";
+            button24.Size = new System.Drawing.Size(92, 68);
+            button24.TabIndex = 23;
+            button24.Text = "=";
+            button24.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.Location = new System.Drawing.Point(15, 95);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(378, 70);
+            label1.TabIndex = 24;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.Location = new System.Drawing.Point(15, 25);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(378, 46);
+            label2.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(402, 720);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button24);
+            Controls.Add(button23);
+            Controls.Add(button22);
+            Controls.Add(button21);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calculator";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
